d2a570ae46915a007604704848d179cbb6077a56
andycarlin <131100433+andycarlin@users.noreply.github.com>
2023-05-22 21:42:59 -0500

    first commit

---
+++ store-examen-final/backend/order.php
@@ -0,0 +1,36 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+require_once 'Connection.php';
+$connection = new Connection();
+$rawData = file_get_contents('php://input');
+$order = json_decode($rawData);
+
+$date = new DateTime();
+$now = $date->format('Y-m-d');
+
+
+
+$id = $order->userId;
+
+// 9. Generar la sentencia SQL para insertar los valores del id de la orden y la variable now en la tabla orders
+$query = "INSERT INTO orders (userId, date)
+            VALUES ('$id', '$now')";
+$result = $connection->query($query);
+
+// --
+// 9. Realizar la consulta a la base de datos y guardar su resultado en la variable $result
+
+// --
+if($result === null){
+    echo "Error al crear el pedido";
+}
+$orderId = $connection->getLastId();
+
+foreach($order->products as $product){
+    $query = "INSERT INTO order_details (orderId, productId, quantity)
+        VALUES($orderId, {$product->id}, 1)";
+    $connection->query($query);
+}
+
+
+echo( json_encode(["message" => "Se ha registrado el usuario"]) );
